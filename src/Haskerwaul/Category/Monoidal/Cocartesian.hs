b3dada6d0d464f3440d29ae439c5a3b1554e59ea
Greg Pfeil <greg.pfeil@kittyhawk.aero>
2020-09-17 11:19:24 -0600

    Add `codiagonal` morphism.

--- src/Haskerwaul/Category/Monoidal/Cocartesian.hs
+++ src/Haskerwaul/Category/Monoidal/Cocartesian.hs
@@ -9,7 +9,22 @@ module Haskerwaul.Category.Monoidal.Cocartesian
 import Haskerwaul.Category.Monoidal.Cartesian
 import Haskerwaul.Category.Opposite
 
--- | [nLab](https://ncatlab.org/nlab/show/cocartesian+monoidal+category)
+-- | We provide an unwrapped version of `diagonal` as `codiagonal`, but the
+--   [coprojections](https://ncatlab.org/nlab/show/coprojection) generally have
+--   the same name as the
+--   [projections](https://ncatlab.org/nlab/show/projection), so there's not
+--   (yet) an unwrapped version of those.
+--
+-- = references
+--
+-- - [nLab](https://ncatlab.org/nlab/show/cocartesian+monoidal+category)
 type CocartesianMonoidalCategory c = CartesianMonoidalCategory (Opposite c)
 
 type Coprod c = Prod (Opposite c)
+
+-- |
+-- = references
+--
+-- - [nLab](https://ncatlab.org/nlab/show/codiagonal)
+codiagonal :: (CocartesianMonodialCategory c, Ob c a) => Prod c a a `c` a
+codiagonal = opposite diagonal
