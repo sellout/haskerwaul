bc51dfd1703358ef2b04301e5144d6b633028237
Greg Pfeil <greg.pfeil@kittyhawk.aero>
2020-09-30 09:09:15 -0600

    Initial work on connecting to Hedgehog for tests.

---
+++ hedgehog/Haskerwaul/Hedgehog.hs
@@ -0,0 +1,164 @@
+{-# language OverloadedStrings
+           , TypeApplications
+           , UndecidableInstances #-}
+{-# options_ghc -Wwarn #-}
+
+-- | This wraps the laws from Haskerwaul in a way that makes them testable with
+--   Hedgehog.
+--
+--   The equality relation for Hedgehog requires `Show` instances, but if an
+--   input type isn't used as a result type in a test, then we accept an
+--   explicit display function for the input type instead of relying on `show`.
+--
+--   Even though some laws may have multiple @translate@-style parameters, they
+--   should generally be the same term for a particular category.
+--
+-- = Structure of @_laws@ definitions
+--
+-- 1. a label to include on any properties
+-- 2. the underlying @Laws@ definition to use
+-- 3. one homomorphism to `HH` for each property "shape",
+-- 4. `Eq` and `Show` instances for each property result type,
+-- 5. one display function for each input-only type (otherwise, they use `show` implicitly), and
+-- 6. one generator for each input type (regardless of whether it's an output type).
+module Haskerwaul.Hedgehog where
+
+import Control.Monad ((=<<))
+import Data.Constraint.Deferrable ((:~:)(..))
+import Data.Eq (Eq)
+import Data.Proxy (Proxy (..))
+import Data.String (String, fromString)
+import Data.Typeable (Typeable, typeRep)
+import Haskerwaul.Category.Laws
+import Haskerwaul.Category.Semigroupal.Laws as Semigroupal
+import Haskerwaul.Hedgehog.Topos
+import Haskerwaul.Isomorphism.Laws
+import Haskerwaul.Law
+import Haskerwaul.Magma.Unital.Laws
+import Haskerwaul.Monoid.Laws
+import Haskerwaul.Object
+import Haskerwaul.Semigroup.Laws as Semigroup
+import Haskerwaul.Semigroupoid.Laws
+import Haskerwaul.Topos.Elementary hiding (leftIdentity, rightIdentity)
+import Haskerwaul.Transformation.Dinatural
+import Hedgehog hiding (label)
+import Text.Show (Show (..))
+
+showType :: forall a. Typeable a => Proxy a -> PropertyName
+showType = fromString . show . typeRep
+
+semigroup_laws
+  :: forall c t a x y. (Typeable c, Eq y, Show y)
+  => PropertyName
+  -> SemigroupLaws c t a
+  -> (t (t a a) a `c` a -> x `HH` y)
+  -> (x -> String)
+  -> Gen x
+  -> [(PropertyName, Property)]
+semigroup_laws label law translate display genX =
+  [( "associative (" <> label <> ")"
+   , property (runHH (checkLaw translate (Semigroup.associative law)) =<< forAllWith display genX))
+  ]
+
+unitalMagma_laws
+  :: (Eq y, Show y)
+  => PropertyName
+  -> UnitalMagmaLaws c t a
+  -> (t (Unit c t) a `c` a -> x1 `HH` y)
+  -> (t a (Unit c t) `c` a -> x2 `HH` y)
+  -> (x1 -> String)
+  -> (x2 -> String)
+  -> Gen x1
+  -> Gen x2
+  -> [(PropertyName, Property)]
+unitalMagma_laws label law transL transR dispL dispR genX1 genX2 =
+  [ ( "left identity (" <> label <> ")"
+    , property (runHH (checkLaw transL (leftIdentity law)) =<< forAllWith dispL $ genX1))
+  , ( "right identity (" <> label <> ")"
+    , property (runHH (checkLaw transR (rightIdentity law)) =<< forAllWith dispR $ genX2))
+  ]
+
+monoid_laws
+  :: (Typeable c, Eq y, Show y)
+  => PropertyName
+  -> MonoidLaws c t a
+  -> (t (t a a) a `c` a -> x `HH` y)
+  -> (t (Unit c t) a `c` a -> x1 `HH` y)
+  -> (t a (Unit c t) `c` a -> x2 `HH` y)
+  -> (x -> String)
+  -> (x1 -> String)
+  -> (x2 -> String)
+  -> Gen x
+  -> Gen x1
+  -> Gen x2
+  -> [(PropertyName, Property)]
+monoid_laws label law trans transL transR display dispL dispR genX genX1 genX2 =
+  semigroup_laws label (semigroup law) trans display genX
+  <> unitalMagma_laws label (unitalMagma law) transL transR dispL dispR genX1 genX2
+
+semigroupoid_laws
+  :: forall ok (c :: ok -> ok -> *) (a :: ok) (b :: ok) y
+   . (Typeable ok, Eq y, Show y)
+  => PropertyName
+  -> SemigroupoidLaws c
+  -> (a `c` b -> y)
+  -> (Procompose (Procompose c c) c a b -> String)
+  -> Gen (Procompose (Procompose c c) c a b)
+  -> [(PropertyName, Property)]
+semigroupoid_laws label law post = semigroup_laws label law (HH . (post .) . runDT)
+
+category_laws
+  :: forall ok (c :: ok -> ok -> *) (a :: ok) (b :: ok) y
+   . (Typeable ok, Eq y, Show y)
+  => PropertyName
+  -> CategoryLaws c
+  -> (a `c` b -> y)
+  -> (Procompose (Procompose c c) c a b -> String)
+  -> (Procompose (:~:) c a b -> String)
+  -> (Procompose c (:~:) a b -> String)
+  -> Gen (Procompose (Procompose c c) c a b)
+  -> Gen (Procompose (:~:) c a b)
+  -> Gen (Procompose c (:~:) a b)
+  -> [(PropertyName, Property)]
+category_laws label law post =
+  monoid_laws label law (HH . (post .) . runDT) (HH . (post .) . runDT) (HH . (post .) . runDT)
+
+semigroupalCategory_laws
+  :: forall ok (c :: ok -> ok -> *) (t :: ok -> ok -> ok) (a :: ok) (b :: ok) m n o x3 x4 y
+   . (Typeable ok, Ob c m, Ob c n, Ob c o, Eq y, Show y, Eq x3, Show x3, Eq x4, Show x4)
+  => PropertyName
+  -> SemigroupalCategoryLaws c t
+  -> (t m (t n o) `c` t m (t n o) -> x3 `HH` x3)
+  -> (t (t m n) o `c` t (t m n) o -> x4 `HH` x4)
+  -> (a `c` b -> y)
+  -> (Procompose (Procompose c c) c a b -> String)
+  -> (Procompose (:~:) c a b -> String)
+  -> (Procompose c (:~:) a b -> String)
+  -> Gen (Procompose (Procompose c c) c a b)
+  -> Gen (Procompose (:~:) c a b)
+  -> Gen (Procompose c (:~:) a b)
+  -> Gen x3
+  -> Gen x4
+  -> [(PropertyName, Property)]
+semigroupalCategory_laws label law trans1 trans2 post display dispL dispR genX genX1 genX2 genX3 genX4 =
+  category_laws (label <> " category") (Semigroupal.category law) post display dispL dispR genX genX1 genX2
+  <> isomorphism_laws (label <> " associative tensor") (Semigroupal.associative law) trans1 trans2 genX3 genX4
+
+isomorphism_laws
+  :: (Eq x1, Show x1, Eq x2, Show x2)
+  => PropertyName
+  -> IsomorphismLaws c a b
+  -> (a `c` a -> x1 `HH` x1)
+  -> (b `c` b -> x2 `HH` x2)
+  -> Gen x1
+  -> Gen x2
+  -> [(PropertyName, Property)]
+isomorphism_laws label law trans1 trans2 genX1 genX2 =
+  [ ( "forward isomorphism (" <> label <> ")"
+    , property (runHH (checkLaw trans1 (forward law)) =<< forAll genX1))
+  , ( "backward isomorphism (" <> label <> ")"
+    , property (runHH (checkLaw trans2 (backward law)) =<< forAll genX2))
+  ]
+
+-- Would love to have a function like
+-- forall a b y. (Eq y, Show y) => a `c` b -> 
---
+++ hedgehog/Haskerwaul/Hedgehog/Topos.hs
@@ -0,0 +1,99 @@
+{-# language UndecidableInstances #-}
+
+-- | This defines an `ElementaryTopos` instance for Hedgehog, such that it can
+--   be used to apply all of the properties defined in Haskerwaul.
+module Haskerwaul.Hedgehog.Topos where
+
+import Control.Applicative
+import Data.Eq (Eq)
+import Data.Type.Equality ((:~:)(..))
+import Haskerwaul.Bifunctor
+import Haskerwaul.Constraint
+import Haskerwaul.Isomorphism
+import Haskerwaul.Object
+import Haskerwaul.Object.Terminal
+import Haskerwaul.Relation.Equality
+import Haskerwaul.Topos.Elementary
+import Haskerwaul.Transformation.Dinatural
+import Hedgehog
+import Text.Show (Show)
+import System.IO
+
+-- | A trivial newtype over __Hask__ that lets us choose a different classifying
+--   object (i.e., `Property`-like instead of `Bool`).
+--
+--   It would be even better if we could treat every input like a generator, e.g., @runHH :: Gen a -> b@
+newtype HH a b = HH { runHH :: a -> b }
+
+type instance Ob HH = All
+
+instance Magma (DinaturalTransformation (->)) Procompose HH where
+  op = DT (\(Procompose (HH f) (HH g)) -> HH (runDT op (Procompose f g)))
+  
+instance Semigroup (DinaturalTransformation (->)) Procompose HH where
+
+instance UnitalMagma (DinaturalTransformation (->)) Procompose HH where
+  unit p = DT (\Refl -> HH (runDT (unit p) Refl))
+
+instance SemigroupalCategory (->) t => SemigroupalCategory HH t where
+  assoc = Iso (HH (to assoc)) (HH (from assoc))
+
+instance MonoidalCategory' (->) t => MonoidalCategory' HH t where
+  type Unit HH t = Unit (->) t
+
+instance MonoidalCategory (->) t => MonoidalCategory HH t where
+  leftIdentity = Iso (HH (to leftIdentity)) (HH (from leftIdentity))
+  rightIdentity = Iso (HH (to rightIdentity)) (HH (from rightIdentity))
+
+instance BraidedMonoidalCategory (->) t => BraidedMonoidalCategory HH t where
+  braid = HH braid
+
+instance SymmetricMonoidalCategory (->) t => SymmetricMonoidalCategory HH t
+
+instance HasTerminalObject HH where
+  type TerminalObject HH = ()
+  (!) = HH (!)
+
+instance CartesianMonoidalCategory HH where
+  type Prod HH = (,)
+  exl = HH exl
+  exr = HH exr
+  diagonal = HH diagonal
+
+instance ClosedCategory HH where
+  type Exp HH = (->)
+
+instance ClosedMonoidalCategory (->) t => ClosedMonoidalCategory HH t where
+  apply = HH apply
+
+instance CartesianClosedCategory HH where
+  curry = Iso (HH . to curry . runHH) (HH . from curry . runHH)
+  tuple = HH tuple
+
+instance ElementaryTopos HH where
+  type Class HH = PropertyT IO ()
+  true = HH pure
+
+instance Bifunctor (->) (->) (->) f => Bifunctor HH HH HH f where
+  bimap (HH f) (HH g) = HH (bimap f g)
+
+instance Magma (->) t a => Magma HH t a where
+  op = HH op
+
+instance Semigroup (->) t a => Semigroup HH t a
+
+instance UnitalMagma (->) t a => UnitalMagma HH t a where
+  unit p = HH (unit p)
+
+-- | The relation hierarchy in Haskerwaul is odd. But, for now, all we care
+--   about in the `HH` topos is equivalence, so we push everything through that.
+instance (Eq a, Show a) => HomogeneousRelation HH a where
+  rel = HH (from curry (===))
+
+instance (Eq a, Show a) => Preorder HH a
+
+instance (Eq a, Show a) => PartialOrder HH a
+
+instance (Eq a, Show a) => PartialEquivalenceRelation HH a
+
+instance (Eq a, Show a) => ToleranceRelation HH a
